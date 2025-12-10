33dc16d286e21c2921650145921a411d3687a3af
Александр Абрамов <aabramov06@yandex.ru>
2025-12-10 11:10:37 +0500

    Initial commit for develop

--- tests.py
+++ tests.py
@@ -18,7 +18,18 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+   
+    # проверка первоначального состояния
+    def test_initial_state(self):
+        collector = BooksCollector()
+        assert collector.get_books_genre() == {} and collector.get_list_of_favorites_books() == []
+
+        # проверка добавления книги в словарь без жанра
+    def test_add_new_book_correct_add_book_successful_add(self):
+        collector = BooksCollector()
+        collector.add_new_book('Азбука')
+        assert collector.get_book_genre('Азбука') == ''
